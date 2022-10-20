af4e7e14d935010b472617dcc6d8d4f8c16c5f9e
João Guilherme <jguilherme.1997@gmail.com>
2022-10-20 16:57:49 -0300

    Ajustes visuais

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
+
 class ApplicationController < ActionController::Base
+    include Pagy::Backend
+
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+    def not_found
+        flash[:error] = "Erro ao localizar #{exception.model} com ID = #{expection.id}"
+        redirect_to action: :index
+    end
 end
